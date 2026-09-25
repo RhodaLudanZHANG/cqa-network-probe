37222af1385d2b8163799ba80f4f441554504635
Rhoda Ludan ZHANG <rhodazhang10x2@gmail.com>
2026-09-25 14:31:13 +0400

    Add GitHub Pages transport for school canary

---
+++ firebase-native-canary/config.js
@@ -0,0 +1,9 @@
+window.__CQA_CANARY_CONFIG__ = Object.freeze({
+  "mode": "cloud-canary",
+  "apiKey": "AIzaSyBEwL_mfbzn66R6mrlLFYqSL-nVjNlblZk",
+  "authDomain": "cqa-school-canary.firebaseapp.com",
+  "projectId": "cqa-school-canary",
+  "storageBucket": "cqa-school-canary.firebasestorage.app",
+  "messagingSenderId": "52814386346",
+  "appId": "1:52814386346:web:f884871711511bbabeecd3"
+});
